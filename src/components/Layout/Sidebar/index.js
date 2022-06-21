66478c2c58d13b162b79d7744afa70af5ab916a3
SurPhrog <102181741+SpookyBugie@users.noreply.github.com>
2022-06-21 12:00:18 +0200

    sidebar details

--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -1,6 +1,11 @@
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../../assets/images/logo-s.png' 
+import LogoS from '../../../assets/images/logo-v.png' 
+import LogoSubtitle from '../../../assets/images/Logo.png' 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from  '@fortawesome/free-brands-svg-icons'
 
 
 
@@ -8,7 +13,37 @@ const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoS} alt= 'logo' />
+            <img className='sub-logo' src={LogoSubtitle} alt= 'Victor' />
         </Link>
+        <nav>
+                <NavLink exact='true' activeclassname='active' to='/'>
+                    <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+                </NavLink>
+
+                <NavLink exact='true' activeclassname='active' className='about-link' to='/about'>
+                    <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+                </NavLink>
+                
+                <NavLink exact='true' activeclassname='active' className='concatact-link' to='/contact'>
+                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+                </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/victor-figueroa-02b53216a'>
+                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                </a>
+            </li>
+
+            
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://github.com/SpookyBugie'>
+                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                </a>
+            </li>
+
+            
+        </ul>
     </div>
 )
 
